17461da2b38d34c0156a8752b9dd0205ae4800c8
Krish Rathor <krishrathor18@gmail.com>
2024-10-04 15:32:39 +0530

    designed campaign dashboard

--- apps/webapp/src/components/campaign-dashboard/CampaignLayout.tsx
+++ apps/webapp/src/components/campaign-dashboard/CampaignLayout.tsx
@@ -1,9 +1,101 @@
 import React from "react";
+import CampaignSwitcher from "./CampaignSwitcher";
+import { Separator } from "../ui/separator";
+import { CalendarDateRangePicker } from "./DateRangePicker";
+import { Tabs, TabsList, TabsTrigger } from "../ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Overview } from "./Overview";
+import { RecentSales } from "./RecentSales";
 
 export const CampaignDashboard: React.FC = () => {
-    return (
-        <div>
-            campaign dashbaord
+  return (
+    <div className="w-[80%] mx-auto">
+      <Nav />
+      <Hero />
+      <div className="flex justify-evenly flex-wrap" >
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
+      <div className="flex justify-evenly" >
+        <Overview />
+        <RecentSales />
+      </div>
+    </div>
+  );
+};
+
+const Nav: React.FC = () => {
+  return (
+    <div>
+      <div className="flex items-center mt-4">
+        <CampaignSwitcher />
+        <div className="mx-3 cursor-pointer ">Overview</div>
+        <div className="mx-3 text-gray-400 cursor-pointer">Nft's Launch</div>
+        <div className="mx-3 text-gray-400 cursor-pointer">Social</div>
+        <div className="mx-3 text-gray-400 cursor-pointer">Launch Page</div>
+      </div>
+      <Separator className="mt-3" />
+    </div>
+  );
+};
+
+const Hero: React.FC = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <p className="text-3xl ml-2 font-bold mt-5">Dashboard</p>
+        <div className="mt-5 mr-2">
+          <CalendarDateRangePicker />
         </div>
-    )
-}
+      </div>
+      <div>
+        <Tabs defaultValue="all" className="w-full h-full space-y-6 mt-4 ml-4">
+          <div className="space-between justify-between flex items-center">
+            <TabsList>
+              <TabsTrigger value="all" className="relative">
+                All
+              </TabsTrigger>
+              <TabsTrigger value="ongoing">OnGoing</TabsTrigger>
+              <TabsTrigger value="past">Past</TabsTrigger>
+              <TabsTrigger value="past">Past</TabsTrigger>
+            </TabsList>
+          </div>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+const Item: React.FC = () => {
+  return (
+    <Card className="bg-[#09090B] w-[18vw] h-[16vh] mt-4 text-white border border-[#27272A]" >
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          className="h-4 w-4 text-muted-foreground"
+        >
+          <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+        </svg>
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">$45,231.89</div>
+        <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+      </CardContent>
+    </Card>
+  );
+};
